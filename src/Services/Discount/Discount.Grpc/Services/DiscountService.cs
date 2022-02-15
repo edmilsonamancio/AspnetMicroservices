ca957eb4c8ae95b2c88526d541234ac46de192c7
Edmilson Amancio <edmilson.aamancio@gmail.com>
2022-02-14 21:59:37 -0300

    Aula - 67. Implementing AutoMapper into DiscountService Class of Discount Grpc Microservice

--- src/Services/Discount/Discount.Grpc/Services/DiscountService.cs
+++ src/Services/Discount/Discount.Grpc/Services/DiscountService.cs
@@ -1,4 +1,6 @@
-﻿using Discount.Grpc.Protos;
+﻿using AutoMapper;
+using Discount.Grpc.Entities;
+using Discount.Grpc.Protos;
 using Discount.Grpc.Repositories;
 using Grpc.Core;
 using Microsoft.Extensions.Logging;
@@ -12,22 +14,25 @@ namespace Discount.Grpc.Services
     public class DiscountService : DiscountProtoService.DiscountProtoServiceBase
     {
         private readonly IDiscountRepository _repository;
+        private readonly IMapper _mapper;
         private readonly ILogger<DiscountService> _logger;
 
-        public DiscountService(IDiscountRepository repository, ILogger<DiscountService> logger)
+        public DiscountService(IDiscountRepository repository, IMapper mapper, ILogger<DiscountService> logger)
         {
             _repository = repository ?? throw new ArgumentNullException(nameof(repository));
+            _mapper = mapper ?? throw new ArgumentNullException(nameof(mapper));
             _logger = logger ?? throw new ArgumentNullException(nameof(logger));
         }
 
         public override async Task<CouponModel> GetDiscount(GetDiscountResquest request, ServerCallContext context)
         {
-            var coupon = _repository.GetDiscount(request.ProductName);
+            var coupon = await _repository.GetDiscount(request.ProductName);
 
             if (coupon == null)
             {
                 throw new RpcException(new Status(StatusCode.NotFound, $"Discount with ProductName={ request.ProductName } is not found."));
             }
+            _logger.LogInformation("Discount is retrieved for ProductName : {productName}, Amount : {Amount}", coupon.ProductName, coupon.Amount);
 
             var couponModel = _mapper.Map<CouponModel>(coupon);
             return couponModel;
